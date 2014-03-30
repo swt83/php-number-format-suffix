638453bb701403f1a1d46ecf5ca4f8312013888a
Scott Travis <scott.w.travis@gmail.com>
2014-03-29 20:36:00 -0500

    Fix formatting.

--- src/number_format_suffix.php
+++ src/number_format_suffix.php
@@ -5,17 +5,15 @@ if (!function_exists('number_format_suffix'))
     function number_format_suffix($num, $html_mode = false)
     {
         // format suffix
-        $suffix = null;
-        if (!in_array(($num % 100), array(11,12,13)))
-        {
-            switch ($num % 10)
-            {
-                case 1: $suffix = 'st';
-                case 2: $suffix = 'nd';
-                case 3: $suffix = 'rd';
-            }
-        }
-        if (!$suffix) $suffix = 'th';
+        $ends = array('th', 'st', 'nd', 'rd', 'th', 'th', 'th', 'th', 'th', 'th');
+		if (($num % 100) >= 11 and ($num % 100) <= 13)
+		{
+		   $suffix = 'th';
+		}
+		else
+		{
+		   $suffix = $ends[$num % 10];
+		}
 
         // format number
         $num = number_format($num);
